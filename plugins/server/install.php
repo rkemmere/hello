fcf325620b11a241279c7a97a2026ca5384be073
Alexander Walther <github.com@alexplus.de>
2018-07-10 16:32:15 +0200

    unique key und updatedate beim Server

--- plugins/server/install.php
+++ plugins/server/install.php
@@ -3,10 +3,11 @@
 rex_sql_table::get(rex::getTable('hello_domain'))
     ->ensureColumn(new rex_sql_column('id', 'int(11)', false, null, 'auto_increment'))
     ->ensureColumn(new rex_sql_column('domain', 'text'))
-    ->ensureColumn(new rex_sql_column('url', 'text'))
     ->ensureColumn(new rex_sql_column('api_key', 'text'))
     ->ensureColumn(new rex_sql_column('createdate', 'timestamp', false, '0000-00-00 00:00:00', 'on update CURRENT_TIMESTAMP'))
+    ->ensureColumn(new rex_sql_column('updatedate', 'timestamp', false, '0000-00-00 00:00:00'))
     ->setPrimaryKey('id')
+    ->ensureIndex(new rex_sql_index('domain', ['domain'], rex_sql_index::UNIQUE))
     ->ensure();
 
     rex_sql_table::get(rex::getTable('hello_domain_log'))
