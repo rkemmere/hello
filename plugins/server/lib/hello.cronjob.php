0fffbe2e34d0bfe8bb1368717f994d82feb2ed59
Alexander Walther <github.com@alexplus.de>
2018-07-10 16:34:00 +0200

    Cronjob-Rundruf für bis zu 50 Installationen
    curl_multi_init(), um möglichst viele Installationen gleichzeitig abzurufen, ohne einen Timeout zu generieren.
    
    Außerdem Loggen, welche Domains als letztes abgerufen wurden, um die nächsten anzustoßen.

--- plugins/server/lib/hello.cronjob.php
+++ plugins/server/lib/hello.cronjob.php
@@ -6,24 +6,50 @@ class rex_cronjob_hello extends rex_cronjob
     public function execute()
     {
 
-        $domains = rex_sql::factory()->setDebug(0)->getArray("SELECT * FROM rex_hello_domain"); 
+        $domains = rex_sql::factory()->setDebug(0)->getArray("SELECT * FROM rex_hello_domain ORDER BY updatedate asc LIMIT 50"); 
+
+        $multi_curl = curl_multi_init();
+        $resps = array();
+        $options = array(
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_AUTOREFERER    => true, 
+            CURLOPT_HEADER         => false,
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_TIMEOUT => 1000
+        );
+        $fstreams = array();
 
         foreach($domains as $domain) {
-        $curl = curl_init();
-        $url = $domain['url']."/?rex-api-call=hello&api_key=".$domain['api_key'];
-        curl_setopt_array($curl,array(CURLOPT_URL => $url, CURLOPT_RETURNTRANSFER => true));
-        $resp = curl_exec($curl);
-
-            if (!curl_errno($curl)) { 
-
-                if(json_last_error() === JSON_ERROR_NONE) {
-                    rex_sql::factory()->setDebug(0)->setQuery('INSERT INTO rex_hello_domain_log (`domain`, `status`, `createdate`, `raw`) VALUES(?,?,NOW(),?)', [$domain['domain'], 1, $resp] );
-                } else {
-                    rex_sql::factory()->setDebug(0)->setQuery('INSERT INTO rex_hello_domain_log (`domain`, `status`, `createdate`, `raw`) VALUES(?,?,NOW(),?)', [$domain['domain'], 0, $resp] );
-                }
-            }
-
+            $i = $domain['domain'];
+            $url = $domain['domain']."/?rex-api-call=hello&api_key=".$domain['api_key'];
+            $resps[$i] = curl_init($url);
+            curl_setopt_array($resps[$i], $options);
+            curl_multi_add_handle($multi_curl, $resps[$i]);
         }
+        $active = null;
+        do {
+            curl_multi_exec($multi_curl, $active);
+        } while ($active > 0);
+
+         
+        foreach ($resps as $key => $response) {
+
+            $resp = curl_multi_getcontent($response);
+            curl_multi_remove_handle($multi_curl, $response);
+            //curl_close($response);
+
+            $json = json_decode($resp);
+
+            if(json_last_error() === JSON_ERROR_NONE) {
+                rex_sql::factory()->setDebug(0)->setQuery('INSERT INTO rex_hello_domain_log (`domain`, `status`, `createdate`, `raw`) VALUES(?,?,NOW(),?)', [$key, 1, $resp] );
+            } else {
+                rex_sql::factory()->setDebug(0)->setQuery('INSERT INTO rex_hello_domain_log (`domain`, `status`, `createdate`, `raw`) VALUES(?,?,NOW(),?)', [$key, 0, $resp] );
+            }
+            rex_sql::factory()->setDebug(0)->setQuery("UPDATE rex_hello_domain SET updatedate = NOW() WHERE domain = ?", [$key]); 
+        }
+        
+        curl_multi_close($multi_curl);
 
         return true;
 
