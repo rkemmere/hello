f9e12aad772f3ea67ad01ffa49a83c20eeb2d8fb
Alexander Walther <github.com@alexplus.de>
2018-07-10 16:58:29 +0200

    key-Länge festlegen für unique

--- plugins/server/install.php
+++ plugins/server/install.php
@@ -2,7 +2,7 @@
 
 rex_sql_table::get(rex::getTable('hello_domain'))
     ->ensureColumn(new rex_sql_column('id', 'int(11)', false, null, 'auto_increment'))
-    ->ensureColumn(new rex_sql_column('domain', 'text'))
+    ->ensureColumn(new rex_sql_column('domain', 'varchar(255)'))
     ->ensureColumn(new rex_sql_column('api_key', 'text'))
     ->ensureColumn(new rex_sql_column('createdate', 'timestamp', false, '0000-00-00 00:00:00', 'on update CURRENT_TIMESTAMP'))
     ->ensureColumn(new rex_sql_column('updatedate', 'timestamp', false, '0000-00-00 00:00:00'))
